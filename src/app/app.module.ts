99ab9caf512047294311efd15e59229b89cb7d87
Yu-Chun <dyj0183@byui.edu>
2021-03-22 10:17:42 -0600

    updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MenuListComponent } from './menus/menu-list/menu-list.component';
+import { MenuItemComponent } from './menus/menu-item/menu-item.component';
+import { MenuDetailComponent } from './menus/menu-detail/menu-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuListComponent,
+    MenuItemComponent,
+    MenuDetailComponent
   ],
   imports: [
     BrowserModule
